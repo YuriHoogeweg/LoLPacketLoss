f5d4f2c83becfcfaaacf64700d667b1d39664413
Yuri Hoogeweg <yurihoogeweg@gmail.com>
2014-10-12 17:26:12 +0200

    Initial commit

---
+++ AveragePacketLoss/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace AveragePacketLoss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblPeakValue = new System.Windows.Forms.Label();
+            this.lblAverageValue = new System.Windows.Forms.Label();
+            this.lblTotalValue = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblPeak = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(12, 25);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(379, 20);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.Text = "C:\\Riot Games\\League of Legends\\Logs\\Network Logs\\";
+            // 
+            // lblPath
+            // 
+            this.lblPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 9);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(236, 13);
+            this.lblPath.TabIndex = 99;
+            this.lblPath.Text = "Your League of Legends Network Logs file path:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(397, 21);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(86, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(0, 3);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(86, 23);
+            this.btnStart.TabIndex = 100;
+            this.btnStart.Text = "Calculate";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Location = new System.Drawing.Point(12, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(379, 117);
+            this.panel1.TabIndex = 101;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lblPeakValue);
+            this.groupBox1.Controls.Add(this.lblAverageValue);
+            this.groupBox1.Controls.Add(this.lblTotalValue);
+            this.groupBox1.Controls.Add(this.lblTotal);
+            this.groupBox1.Controls.Add(this.lblPeak);
+            this.groupBox1.Controls.Add(this.lblAverage);
+            this.groupBox1.Location = new System.Drawing.Point(92, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 101);
+            this.groupBox1.TabIndex = 101;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Values";
+            // 
+            // lblPeakValue
+            // 
+            this.lblPeakValue.AutoSize = true;
+            this.lblPeakValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblPeakValue.Location = new System.Drawing.Point(215, 70);
+            this.lblPeakValue.Name = "lblPeakValue";
+            this.lblPeakValue.Size = new System.Drawing.Size(0, 13);
+            this.lblPeakValue.TabIndex = 5;
+            // 
+            // lblAverageValue
+            // 
+            this.lblAverageValue.AutoSize = true;
+            this.lblAverageValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblAverageValue.Location = new System.Drawing.Point(215, 45);
+            this.lblAverageValue.Name = "lblAverageValue";
+            this.lblAverageValue.Size = new System.Drawing.Size(0, 13);
+            this.lblAverageValue.TabIndex = 4;
+            // 
+            // lblTotalValue
+            // 
+            this.lblTotalValue.AutoSize = true;
+            this.lblTotalValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTotalValue.Location = new System.Drawing.Point(215, 20);
+            this.lblTotalValue.Name = "lblTotalValue";
+            this.lblTotalValue.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalValue.TabIndex = 3;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(7, 20);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(37, 13);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "Total: ";
+            // 
+            // lblPeak
+            // 
+            this.lblPeak.AutoSize = true;
+            this.lblPeak.Location = new System.Drawing.Point(7, 70);
+            this.lblPeak.Name = "lblPeak";
+            this.lblPeak.Size = new System.Drawing.Size(35, 13);
+            this.lblPeak.TabIndex = 1;
+            this.lblPeak.Text = "Peak:";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(7, 45);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(50, 13);
+            this.lblAverage.TabIndex = 0;
+            this.lblAverage.Text = "Average:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 175);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "LoL Packet Loss";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblPeak;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblPeakValue;
+        private System.Windows.Forms.Label lblAverageValue;
+        private System.Windows.Forms.Label lblTotalValue;
+    }
+}
+
---
+++ AveragePacketLoss/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AveragePacketLoss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            string[] filePaths = GetAllFilesInFolder(this.txtPath.Text);
+            List<int> allDropValues = new List<int>();
+            
+            // Create one list of int values of every dropped packet
+            foreach(string filepath in filePaths){
+                allDropValues.AddRange(GetAllDroppedPackages(filepath));
+            }
+
+            // At this point we have all the individual dropped values and can calculate the average and peak etc.
+            int[] TotalAveragePeak = GetTotalAveragePeak(allDropValues);
+
+            lblTotalValue.Text = TotalAveragePeak[0].ToString();
+            lblAverageValue.Text = TotalAveragePeak[1].ToString();
+            lblPeakValue.Text = TotalAveragePeak[2].ToString();
+        }
+
+        private int[] GetTotalAveragePeak(List<int> allValues)
+        {           
+            int total = 0;
+            int average = 0;
+            int peak = 0;
+
+            // Set Total and Peak
+            foreach(int value in allValues){
+                total += value;
+
+                if(value > peak){
+                    peak = value;
+                }
+            }
+
+            // Set average
+            average = total / allValues.Count;
+
+            return new int[3]{total, average, peak};
+        }
+
+        private string[] GetAllFilesInFolder(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+
+        private int GetAmountOfCommas(string line)
+        {
+            int count = 0;
+
+            foreach (char c in line)
+            {
+                if (c == ',')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private List<int> GetAllDroppedPackages(string path)
+        {
+            List<int> AllDroppedPackages = new List<int>();
+
+            // Get packet loss number from every line that has enough comma's to contain packet loss information            
+            StreamReader file = new StreamReader(path);
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (GetAmountOfCommas(line) > 10)
+                {
+                    string sixthString = line.Split(',')[6];
+                    int n;
+
+                    if(int.TryParse(sixthString, out n) == true){
+                        AllDroppedPackages.Add(Convert.ToInt32(sixthString));
+                    }                    
+                }
+            }
+
+            return AllDroppedPackages;
+        }
+    }
+}
